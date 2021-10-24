7e6fa716a3ecbb29561ab3669a69d2e0534b9452
Phạm Ngọc Thùy Trang <43264944+PikarryPham@users.noreply.github.com>
2021-10-24 22:29:22 +0700

    upload db scripts and design code flow

---
+++ 02_createStageDB.sql
@@ -0,0 +1,139 @@
+﻿-- TẠO CSDL CHO DB STAGE
+USE master 
+GO
+IF DB_ID('DALT_STAGE') IS NOT NULL
+	DROP DATABASE DALT_STAGE
+GO
+CREATE DATABASE DALT_STAGE
+GO
+USE DALT_STAGE
+GO
+
+/*
+	Check if table exists? Drop before creating : create
+*/
+
+IF OBJECT_ID(N'dbo.KhachHang_Stage', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.KhachHang_Stage;
+	PRINT 'Delete Table Successfully';
+END;
+IF OBJECT_ID(N'dbo.HoaDon_Stage', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.HoaDon_Stage;
+	PRINT 'Delete Table Successfully';
+END;
+IF OBJECT_ID(N'dbo.ChiTietHoaDon_Stage', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.ChiTietHoaDon_Stage;
+	PRINT 'Delete Table Successfully';
+END;
+IF OBJECT_ID(N'dbo.LoaiSanPham_Stage', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.LoaiSanPham_Stage;
+	PRINT 'Delete Table Successfully';
+END;
+
+IF OBJECT_ID(N'dbo.Nuoc_Stage', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.Nuoc_Stage;
+	PRINT 'Delete Table Successfully';
+END;
+
+IF OBJECT_ID(N'dbo.LoaiCuaHang_Stage', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.LoaiCuaHang_Stage;
+	PRINT 'Delete Table Successfully';
+END;
+
+IF OBJECT_ID(N'dbo.SanPham_Stage', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.SanPham_Stage;
+	PRINT 'Delete Table Successfully';
+END;
+
+IF OBJECT_ID(N'dbo.CuaHang_Stage', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.CuaHang_Stage;
+	PRINT 'Delete Table Successfully';
+END;
+
+/* Create new table */
+CREATE TABLE KhachHang_Stage (
+	[MaKH] [varchar](4) NOT NULL,
+	[TenKH] [varchar](50) NULL,
+	[DiaChi] [varchar](50) NULL,
+	[NgayTao] [datetime] NULL,
+	[NgayCapNhat] [datetime] NULL,
+	[TrangThai] [int] NULL
+);
+
+CREATE TABLE HoaDon_Stage (
+	[MaHD] [varchar](4) NOT NULL,
+	[MaKH] [varchar](4) NULL,
+	[MaCH] [varchar](4) NULL,
+	[NgayBan] [datetime] NULL,
+	[NgayCapNhat] [datetime] NULL,
+	[TrangThai] [int] NULL
+);
+
+CREATE TABLE ChiTietHoaDon_Stage (
+	[MaChiTiet] [int] NOT NULL,
+	[MaHD] [varchar](4) NULL,
+	[MaSP] [varchar](4) NULL,
+	[NgayTao] [datetime] NULL,
+	[NgayCapNhat] [datetime] NULL,
+	[TrangThai] [int] NULL,
+);
+
+CREATE TABLE LoaiSanPham_Stage (
+	[MaLoaiSP] [varchar](4) NOT NULL,
+	[TenLoaiSP] [varchar](50) NULL,
+	[NgayTao] [datetime] NULL,
+	[NgayCapNhat] [datetime] NULL,
+	[TrangThai] [int] NULL
+);
+
+CREATE TABLE Nuoc_Stage (
+	[MaNuoc] [varchar](4) NOT NULL,
+	[TenNuoc] [varchar](50) NULL,
+	[NgayTao] [datetime] NULL,
+	[NgayCapNhat] [datetime] NULL,
+	[TrangThai] [int] NULL
+);
+
+CREATE TABLE LoaiCuaHang_Stage (
+	[MaLoaiCH] [varchar](4) NOT NULL,
+	[TenLoaiCH] [varchar](50) NULL,
+	[NgayTao] [datetime] NULL,
+	[NgayCapNhat] [datetime] NULL,
+	[TrangThai] [int] NULL
+);
+
+CREATE TABLE SanPham_Stage (
+	[MaSP] [varchar](4) NOT NULL,
+	[TenSP] [varchar](50) NULL,
+	[GiaSP] [int] NULL,
+	[LoaiSP] [varchar](4) NULL,
+	[NgayTao] [datetime] NULL,
+	[NgayCapNhap] [datetime] NULL,
+	[TrangThai] [nchar](10) NULL
+);
+
+CREATE TABLE CuaHang_Stage(
+	[MaCH] [varchar](4) NOT NULL,
+	[TenCH] [varchar](50) NULL,
+	[LoaiCH] [varchar](4) NULL,
+	[Nuoc] [varchar](4) NULL,
+	[NgayTao] [datetime] NULL,
+	[NgayCapNhat] [datetime] NULL,
+	[TrangThai] [int] NULL
+);
---
+++ 03_createMetadataDB.sql
@@ -0,0 +1,34 @@
+﻿-- TẠO CSDL CHO DB STAGE
+USE master 
+GO
+IF DB_ID('DALT_METADATA') IS NOT NULL
+	DROP DATABASE DALT_METADATA
+GO
+CREATE DATABASE DALT_METADATA
+GO
+USE DALT_METADATA
+GO
+
+/*
+	Check if table exists? Drop before creating : create
+*/
+
+IF OBJECT_ID(N'dbo.Metadata_Table', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.Metadata_Table;
+	PRINT 'Delete Table Successfully';
+END;
+
+CREATE TABLE Metadata_Table (
+	ID INTEGER IDENTITY(1,1) NOT NULL,
+	Name_DataFlow VARCHAR(512),
+	LSET DATETIME,
+	CET DATETIME
+);
+TRUNCATE TABLE Metadata_Table;
+INSERT INTO Metadata_Table(Name_DataFlow,LSET,CET) VALUES ('KhachHang_Jade','1900-01-01',NULL);
+INSERT INTO Metadata_Table(Name_DataFlow,LSET,CET) VALUES ('HoaDon_Jade','1900-01-01',NULL);
+INSERT INTO Metadata_Table(Name_DataFlow,LSET,CET) VALUES ('ChiTietHoaDon_Jade','1900-01-01',NULL);
+
+SELECT * FROM Metadata_Table;
