686ec133fa2e86cf015c65a483e959a254b0de39
[nt2505] <nth250520@gmail.com>
2021-10-31 12:56:50 +0700

    Add script create stage Webtower tables and ETL source -> stage for Webtower tables

---
+++ 04_createStageDB_Webtower.sql
@@ -0,0 +1,64 @@
+﻿-- TẠO CSDL CHO DB STAGE_Webtower
+USE master 
+GO
+IF DB_ID('DALT_STAGE_Webtower') IS NOT NULL
+	DROP DATABASE DALT_STAGE_Webtower
+GO
+CREATE DATABASE DALT_STAGE_Webtower
+GO
+USE DALT_STAGE_Webtower
+GO
+
+/*
+	Check if table exists? Drop before creating : create
+*/
+
+IF OBJECT_ID(N'dbo.KhachHang_STAGE_Webtower', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.KhachHang_STAGE_Webtower;
+	PRINT 'Delete Table Successfully';
+END;
+IF OBJECT_ID(N'dbo.HoaDon_STAGE_Webtower', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.HoaDon_STAGE_Webtower;
+	PRINT 'Delete Table Successfully';
+END;
+IF OBJECT_ID(N'dbo.ChiTietHoaDon_STAGE_Webtower', N'U') IS NOT NULL
+BEGIN
+    PRINT 'Table Exists';
+	DROP TABLE dbo.ChiTietHoaDon_STAGE_Webtower;
+	PRINT 'Delete Table Successfully';
+END;
+
+/* Create new table */
+CREATE TABLE KhachHang_STAGE_Webtower (
+	[MaKH] varchar(4) NOT NULL,
+  [TenKH] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+  [DiaChi] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+  [NgayTao] datetime NULL,
+  [NgayCapNhat] datetime NULL,
+  [TrangThai] int NULL,
+  [GioiTinh] char(1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+  [SoThich] varchar(30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+  [NgheNghiep] varchar(30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+);
+
+CREATE TABLE HoaDon_STAGE_Webtower (
+	[MaHD] [varchar](4) NOT NULL,
+	[MaKH] [varchar](4) NULL,
+	[MaCH] [varchar](4) NULL,
+	[NgayBan] [datetime] NULL,
+	[NgayCapNhat] [datetime] NULL,
+	[TrangThai] [int] NULL
+);
+
+CREATE TABLE ChiTietHoaDon_STAGE_Webtower (
+	[MaChiTiet] [int] NOT NULL,
+	[MaHD] [varchar](4) NULL,
+	[MaSP] [varchar](4) NULL,
+	[NgayTao] [datetime] NULL,
+	[NgayCapNhat] [datetime] NULL,
+	[TrangThai] [int] NULL,
+);
